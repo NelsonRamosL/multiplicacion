7e0cdcb996065948343b1ec27bff5f6b91ab432b
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-07-07 18:22:58 -0400

    conectado a live share

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
 
-
-
-console.log("entramos en js");
-
-
-
-
+    let cuentaAtras = numero => {
+        //base case
+        if (numero === 0) {
+            return 0;
+        } 
+        console.log(numero);
+        return cuentaAtras(numero - 1);
+    };
+    console.log(cuentaAtras(5)) // 5, 4, 3, 2, 1
+ 
 
 
 
